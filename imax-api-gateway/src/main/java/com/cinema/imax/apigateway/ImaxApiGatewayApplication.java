7374ddef6566f12398d80abb56f39e8415471ad7
Santiago Agustín Bosquet <114213431+PaniaguaSanty@users.noreply.github.com>
2025-09-20 00:30:38 -0300

    feature/api-gateway configuration
    - api gateway parent added.
    - api gateway POM generated.
    - The project is runnable.

---
+++ imax-api-gateway/src/main/java/com/cinema/imax/apigateway/ImaxApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.cinema.imax.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImaxApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImaxApiGatewayApplication.class, args);
+	}
+
+}
