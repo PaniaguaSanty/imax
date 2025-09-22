ee3eb3456eee084cf8424bbf175f5fabbffa1740
Santiago Agustín Bosquet <114213431+PaniaguaSanty@users.noreply.github.com>
2025-09-22 12:50:05 -0300

    config-server microservice created
    - config server dependency added.

---
+++ imax-config-server/src/main/java/com/cinema/imax_config_server/ImaxConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.cinema.imax_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImaxConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImaxConfigServerApplication.class, args);
+	}
+
+}
--- imax-eureka-config/src/main/java/com/cinema/imax_eureka_config/ImaxEurekaConfigApplication.java
+++ imax-eureka-config/src/main/java/com/cinema/imax_eureka_config/ImaxEurekaConfigApplication.java
@@ -2,6 +2,7 @@ package com.cinema.imax_eureka_config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 public class ImaxEurekaConfigApplication {
