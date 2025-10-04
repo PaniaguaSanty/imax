2047a892a860afdf88e80a3a224a2c2547e53935
Santiago Agustín Bosquet <114213431+PaniaguaSanty@users.noreply.github.com>
2025-10-03 22:27:02 -0300

    Add Feign client and reservation model classes
    - Added ShowTimeClient interface using Spring Cloud OpenFeign and a fallback class for client resilience.
    -  Introduced Reservation and ReservationStatus model classes to represent reservation data.
    -  Updated pom.xml to include the spring-cloud-starter-openfeign dependency.

---
+++ imax-reservation-service/src/main/java/com/cinema/imax_reservation_service/client/ReservationClientFallback.java
@@ -0,0 +1,10 @@
+package com.cinema.imax_reservation_service.client;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class ReservationClientFallback {
+    
+}
---
+++ imax-reservation-service/src/main/java/com/cinema/imax_reservation_service/model/ReservationStatus.java
@@ -0,0 +1,8 @@
+package com.cinema.imax_reservation_service.model;
+
+public enum ReservationStatus {
+    PENDING,
+    CONFIRMED,
+    CANCELLED,
+    EXPIRED
+}
